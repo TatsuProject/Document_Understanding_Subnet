c1d457e2bb70bb4635b4914d88cd41637e0efa12
atkinson-ai <beyondblocs@gmail.com>
2025-04-30 13:50:21 +0000

    timeout set to 150 for miners

--- template/validator/forward.py
+++ template/validator/forward.py
@@ -89,7 +89,7 @@ async def forward(self):
         # Send the query to selected miner axons in the network.
         axons=[self.metagraph.axons[uid] for uid in miner_uids],
         synapse=task,
-        timeout=3600,
+        timeout=150,
         # All responses have the deserialize function called on them before returning.
         # You are encouraged to define your own deserialization function.
         deserialize=True,
